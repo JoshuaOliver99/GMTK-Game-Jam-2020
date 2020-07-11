ba91e6fcf83ebbf4e4d9e0aba3576e0e6ce64cfb
JoshuaOliver99 <56636418+JoshuaOliver99@users.noreply.github.com>
2020-07-11 15:33:25 +0100

    Fixed spawner script

--- GMTK Game Jam 2020/Assets/Scripts/AdSpawner.cs
+++ GMTK Game Jam 2020/Assets/Scripts/AdSpawner.cs
@@ -4,31 +4,30 @@ using UnityEngine;
 
 public class AdSpawner : MonoBehaviour
 {
-    private float timer = 0.0f;
+    private float timer = 0.0f; // Spawn timer
+    public GameObject AdPrefab; // referance to the Ad prefab
 
     void Start()
     {
         timer = 0.0f;
-        var loadedObject = Resources.Load("Ad");
     }
 
 
 
     void Update()
     {
-        timer += Time.deltaTime;
-        Debug.Log(timer);
+        timer += Time.deltaTime; // dis
 
         if (timer > 5)
         {
             Debug.Log("5 SECONDS HAS PASSED");
 
+            // ----- SHOULD DO ----
             // SPAWN AN AD PREFAB
             // AD HAS RANDOM AD IMAGE
 
 
-            Instantiate(loadedObject, Vector3.zero, Quaternion.identity); // spawn an ad
-
+            Instantiate(AdPrefab, Vector3.zero, Quaternion.identity); // spawn an ad
 
             timer = 0; // Reset timer
         }
